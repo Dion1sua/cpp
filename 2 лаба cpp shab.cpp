0398008a859aedb83fa4845d6af6e919dbf291be
Dion1sua <153023420+Dion1sua@users.noreply.github.com>
2024-03-11 10:00:38 +0200

    Add files via upload

---
+++ 2 лаба cpp shab.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+T calculateSum(T n) {
+    T sum = 0;
+    for (T i = 20; i <= (2 * n - 1); ++i) {
+        sum += i;
+    }
+    return sum;
+}
+int main() {    
+    setlocale(LC_ALL, "ukr");
+    int n;
+    cout << "Введіть натуральне число n: ";
+    cin >> n;
+    int result = calculateSum(n);
+    cout << "Сума послідовності чисел: " << result << std::endl;
+
+    return 0;
+}
---
+++ срр 2лаба.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+void calculateSumWithPointers(int n, int* sum) {
+    *sum = 0;
+    for (int i = 20; i <= (2 * n - 1); ++i) {
+        *sum += i;
+    }
+}
+int calculateSumWithExternalVariable(int n) {
+    int sum = 0;
+    for (int i = 20; i <= (2 * n - 1); ++i) {
+        sum += i;
+    }
+    return sum;
+}
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int n;
+    cout << "Введіть натуральне число n: ";
+    cin >> n;
+
+    int sumWithPointers;
+    calculateSumWithPointers(n, &sumWithPointers);
+    cout << "Сума послідовності чисел за допомогою параметрів-вказівників: " << sumWithPointers << endl;
+    int sumWithExternalVariable = calculateSumWithExternalVariable(n);
+    cout << "Сума послідовності чисел за допомогою зовнішньої змінної: " << sumWithExternalVariable << endl;
+
+    return 0;
+}
